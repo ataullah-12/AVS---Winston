94070c6b0a06f625517fd42915e6bab9798f6af2
moosashaireff <moosasharieff@gmail.com>
2019-09-12 16:02:46 +0530

    Switching in and out of 'iframe' to corresponde on winston tool.

--- Winston_Code/Main_Winston.py
+++ Winston_Code/Main_Winston.py
@@ -10,6 +10,21 @@ vendor = Excel_utils.readData(url.case_Data, 'Data', 2, 1)
 case = Excel_utils.readData(url.case_Data, 'Data', 2, 2)
 
 driver.get(f"{url.winston}{case}")
-time.sleep(2)
-driver.find_element_by_xpath(f"{url.upload_xpath}").send_keys(f"{url.file_path}{vendor}.xlsx")
-time.sleep(1)
+time.sleep(5)
+# driver.find_element_by_xpath(f"{url.upload_xpath}").send_keys(f"{url.file_path}{vendor}.xlsx")
+# time.sleep(3)
+
+"""
+In the below code we are entering into an iframe to write into correspondence.
+
+iframe_corres=driver.find_element_by_xpath("//iframe[@class='cke_wysiwyg_frame cke_reset']") - Assiging a variable to xpath of an iframe.
+driver.switch_to.frame(iframe_corres) - switching from html to iframe
+driver.find_element_by_tag_name("body").send_keys("musa bhai") - Tying information
+driver.switch_to.default_content() - switching back to html to proceed with further actions. 
+
+"""
+
+iframe_corres=driver.find_element_by_xpath("//iframe[@class='cke_wysiwyg_frame cke_reset']")
+driver.switch_to.frame(iframe_corres)
+driver.find_element_by_tag_name("body").send_keys("musa bhai")
+driver.switch_to.default_content()
